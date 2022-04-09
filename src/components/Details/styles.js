d3210063ba0c527f27f01580f31c8800c83a0225
Ruchika Suryawanshi <ruchikasuryawanshi710@gmail.com>
2022-04-09 11:49:11 +0530

    ss

---
+++ src/components/Details/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  income: {
+    borderBottom: '200px solid rgba(0, 255, 200, 0.5)',
+  },
+  expense: {
+    borderBottom: '200px solid rgba(255, 0, 0, 0.5)',
+  },
+}));
